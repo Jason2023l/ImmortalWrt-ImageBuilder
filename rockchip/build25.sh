7a7f60fe9d0d1ca78d31f797c623854d07ef5460
Jason2023l <128659135+Jason2023l@users.noreply.github.com>
2026-08-12 14:07:31 +0800

    Add additional luci-app packages to build script

--- rockchip/build25.sh
+++ rockchip/build25.sh
@@ -55,6 +55,11 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-app-passwall"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-app-daed"
+PACKAGES="$PACKAGES luci-app-rclone"
+PACKAGES="$PACKAGES luci-app-adguardhome"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
